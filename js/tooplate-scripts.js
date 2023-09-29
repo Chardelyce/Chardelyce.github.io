351277649c45e12b4b3568948367d2c2a888685d
Char * <63970461+Chardelyce@users.noreply.github.com>
2023-09-29 07:27:33 -0500

    fix maybe

--- js/tooplate-scripts.js
+++ js/tooplate-scripts.js
@@ -1,3 +1,197 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particles = [];
+const maxParticles = 100;
+const minDistance = 150;
+
+class Particle {
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.radius = 2;
+        this.velocityX = Math.random() * 4 - 2;
+        this.velocityY = Math.random() * 4 - 2;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {
+        // Update particle position
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        // Bounce off the screen boundaries
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.velocityX = -this.velocityX;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.velocityY = -this.velocityY;
+        }
+
+        this.draw();
+    }
+}
+
+// Create initial particles
+for (let i = 0; i < maxParticles; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const particle = new Particle(x, y, color);
+    particles.push(particle);
+}
+
+function drawLines() {
+    for (let i = 0; i < particles.length; i++) {
+        for (let j = i + 1; j < particles.length; j++) {
+            const dx = particles[i].x - particles[j].x;
+            const dy = particles[i].y - particles[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < minDistance) {
+                ctx.beginPath();
+                ctx.strokeStyle = particles[i].color;
+                ctx.moveTo(particles[i].x, particles[i].y);
+                ctx.lineTo(particles[j].x, particles[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+    }
+
+    drawLines();
+}
+
+animate();
+$(document).ready(function () {
+  $(".scroll-to").on("click", function (event) {
+    event.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(target).offset().top
+    }, 800); // Adjust the animation duration as needed
+  });
+});
+
+$(document).ready(function () {
+  // Select all anchor links except those with the class "no-smooth-scroll"
+  $('a[href*="#"]:not(.no-smooth-scroll)').click(function(event) {
+    event.preventDefault();
+    
+    // Get the target element's ID from the href attribute
+    var target = $(this).attr("href");
+    
+    // Animate the scroll to the target element's position
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 800); // Adjust the animation duration as needed
+  });
+});
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+jQuery(document).ready(function() {
+  "use strict";
+    $('.gallery-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        autoplay: false,
+        dots: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 575,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+        
+    });
+  
+    $(".navbar-button").click(function(e){
+        e.stopPropagation();
+        $(".header").toggleClass("open");
+        $(".navbar-button").toggleClass("collapsed");
+    });
+
+    function closeMenu() {
+      $(".header").removeClass("open");
+      $(".navbar-button").addClass("collapsed"); 
+    }
+
+    $(".navbar .navbar-nav > .nav-item > a.nav-link").click(function(e){
+      e.stopPropagation();
+      closeMenu();     
+    });
+
+    $("html").click(function(e) {
+      closeMenu();
+    });
+
+    $('.single-page-nav').singlePageNav({
+        filter: ':not(.external)',
+        updateHash: true
+    });
+
+
+    
+}
+);
 const width_threshold = 480;
 
 function drawLineChart() {
@@ -190,3 +384,4 @@ function updateBarChart() {
     barChart.update();
   }
 }
+
