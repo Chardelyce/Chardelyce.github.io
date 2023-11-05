da935f799044e59366b8583d199013baf66184d2
Char * <63970461+Chardelyce@users.noreply.github.com>
2023-11-05 16:33:42 -0600

    testing

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -16,7 +16,27 @@ function highlightMenu(no) {
   $(".navbar .navbar-nav > .nav-item").removeClass('selected');
   $(".navbar .navbar-nav > .nav-item > .nav-link[data-no='" + no + "']").parent().addClass('selected');
 }
+const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
+function rotatePlanets() {
+  const currentTime = new Date();
+  const sun = document.getElementById('sun');
 
+  planets.forEach(planet => {
+    const circle = document.getElementById(planet);
+    const orbit = document.getElementById(`orbit-${planet}`);
+
+    const angle = ((currentTime - new Date(0)) / 10000) * (360 / (planet.length * 100));
+    const x = sun.cx.baseVal.value + orbit.rx.baseVal.value * Math.cos(angle);
+    const y = sun.cy.baseVal.value + orbit.ry.baseVal.value * Math.sin(angle);
+
+    circle.cx.baseVal.value = x;
+    circle.cy.baseVal.value = y;
+  });
+
+  requestAnimationFrame(rotatePlanets);
+}
+
+rotatePlanets();
 function setupGallery() {
   gallery = $('.gallery-slider').slick({
     slidesToShow: 5,
