a71ff6b43aa7cf761b36d16e63b09d0fa127aa29
Chardelyce Edwards <chardelycee@gmail.com>
2026-07-08 08:39:29 -0500

    fixing bubblegum

--- .vscode/backend/main.py
+++ .vscode/backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routes import router
 
@@ -10,3 +10,11 @@ Base.metadata.create_all(bind=engine)
 
 # Register API routes
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Development only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
